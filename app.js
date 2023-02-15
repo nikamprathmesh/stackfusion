500e620e316e10ae1e1014ef0a58f9d0720ede31
Prathmesh Nikam <prathameshnikam09@gmail.com>
2023-02-16 01:03:21 +0530

    add build folder

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT =process.env.PORT||  5000;
 var cors = require('cors');
 app.use(cors());
 app.use(express.json());
-const User = require("../models/schema");
+const User = require("./models/schema");
 require("./db/conn");
 // require("../client/public/index.html")
 app.use(express.static(path.join(__dirname,"../client/build")));
