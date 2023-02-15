6ed3bddb169135270735cd702252d04b652ecea5
Prathmesh Nikam <prathameshnikam09@gmail.com>
2023-02-16 01:26:28 +0530

    change schema file

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const dotenv  = require("dotenv");
 const express= require("express");
 const path = require('path');
 const app = express();
+const cors = require('cors');
 const PORT =process.env.PORT||  5000;
 app.use(cors());
 app.use(express.json());
